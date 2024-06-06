b9a3c2a362bfa34b4d69aab139d853947e183185
Lincoln Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2024-06-06 22:16:33 +0800

    Fixing presentation on index.html

--- pages/js/main.js
+++ pages/js/main.js
@@ -1,8 +1,14 @@
 addEventListener('scroll', (evt) => {
-    if (document.documentElement.scrollTop > 70) {
+    if (document.documentElement.scrollTop > document.getElementById('header').style.height) {
         document.getElementById('header').style.backgroundColor = '#214066'
     }
     else {
         document.getElementById('header').style.backgroundColor = 'transparent'
     }
     });
+
+addEventListener('DOMContentLoaded', (evt) => {
+    console.log(window)
+    document.getElementById('title video').height = window.innerHeight
+    document.getElementById('title video').width = window.innerWidth
+});
